e7d54d06fe027765252aac13b3b57d27774a95c6
Khusal143 <khusalkumar26@gmail.com>
2025-03-25 23:10:25 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (void)
+{
+	int even_cnt,num;
+	scanf("%d",&num);
+	for()
+		if(LastDigit % 2 == 0){
+			even_cnt++;
+			
+		
+		}
+		num = num % 2;
+	}printf("%d",num);
+}
